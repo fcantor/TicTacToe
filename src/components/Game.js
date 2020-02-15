02b1df0da32a1c7c78833980f9c647853ddfee14
Francesca <fpcantor@gmail.com>
2020-02-14 16:00:21 -0800

    Add calculateWinner function to Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 import Board from './Board';
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
 export class Game extends Component {
   constructor(props){
     super(props);
@@ -16,6 +38,11 @@ export class Game extends Component {
     const history = this.state.history.slice(0, this.state.stepNumber+1); // copy of previous state
     const current = history[history.length-1];
     const squares = current.squares.slice(); // copy of squares
+    const winner = calculateWinner(squares);
+    if (winner || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext? 'X':'O';
 
     //put new history to the current history
